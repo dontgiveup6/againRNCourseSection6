6162bbc5f388a2ceebf55489e5dd64fe0210111a
Georgi Serafimov <g.serafimov@c-trust.eu>
2022-09-05 15:57:16 +0300

    90-99

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,28 @@
+import { View, FlatList, Text } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
+import { CATEGORIES } from '../data/dummy-data';
+
+export default function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate('MealsOverview', { categoryId: itemData.item.id });
+    }
+
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
+
+  return (
+    <FlatList
+      data={CATEGORIES}
+      renderItem={renderCategoryItem}
+      keyExtractor={(item) => item.id}
+      numColumns={2}
+    />
+  );
+}
---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,37 @@
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import MealItem from '../components/MealItem';
+import { MEALS } from '../data/dummy-data';
+
+// import { useRoute } from '@react-navigation/native';
+
+export default function MealsOverviewScreen({ route }) {
+  // const route=useRoute()
+  //   route.params
+  // alternative way
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((item) => {
+    return item.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMeal(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        renderItem={renderMeal}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
